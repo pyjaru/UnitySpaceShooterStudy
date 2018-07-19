9cdbf52c072dc7d33490370330bd8eeec258379a
pyjaru <pyjaru@gmail.com>
2018-07-19 21:13:24 +0900

    Day2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour 
+{
+	private float nextFire = 0.0f;
+	public float fireRate = 0.5f;
 	public float speed;
+	public float tilt;
+	public Boundary boundary;
+	public GameObject shot;
+	public Transform shotSpawn;
+	void Update() 
+	{
+		// if(Input.GetButton("Fire1") && Time.time > nextFire)
+		if(Input.GetKey(KeyCode.Space) && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
 	void FixedUpdate()
 	{
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 		
+		// 플레이어 이동
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 		GetComponent<Rigidbody>().velocity = movement*speed;
+
+		// 플레이어의 이동범위 설정
+		GetComponent<Rigidbody>().position = new Vector3
+		(
+			Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+		);
+		
+		// 기울기 효과
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
 	}
 }
